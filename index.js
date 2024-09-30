efc19e387626b6dbb8e7a1206009bee8dd2f7704
Dennis Kemboi <denkogei11@gmail.com>
2024-10-01 00:52:54 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(function(record) {
+        return record.result === "W";
+    });
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+const games = [
+    { year: 1997, result: "W", team: "Denver Broncos" },
+    { year: 1998, result: "W", team: "Denver Broncos" }, 
+    { year: 2005, result: "L", team: "Denver Broncos" }, 
+    { year: 2015, result: "W", team: "New England Patriots" }, 
+];
+
+console.log(superbowlWin(games));
